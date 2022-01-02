23021ae6af75c6aaddad7d05d159564c809758c3
runalsh <shaidullin2@gmail.com>
2022-01-02 21:10:09 +0300

    Add files via upload

---
+++ awsecr/ecr.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+}
+
+resource "aws_ecr_repository" "app_repo_go" {
+  name = var.container.go
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "repo_policy" {
+  repository = aws_ecr_repository.app_repo_go.name
+  policy     = file("life_go_ecr.json")
+}
