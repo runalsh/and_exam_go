3381fdc79a17d5e44e4910586af6f0d9c4e070b2
runalsh <shaidullin2@gmail.com>
2022-01-03 17:19:41 +0300

    Delete 1 directory

--- 1/output.tf
+++
@@ -1,3 +0,0 @@
-output "alb_dns" {
-  value = aws_lb.web.dns_name
-}
--- 1/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region                  = var.aws_region
-  shared_credentials_file = "~/.aws/credetials"
-  profile                 = "admin"
-}
-
-
