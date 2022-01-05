ac9a9b07ea40b111b6db48ed82b5349d0be7b798
runalsh <shaidullin2@gmail.com>
2022-01-05 17:22:31 +0300

    Update app.go

--- app.go
+++ app.go
@@ -5,17 +5,17 @@ import (
     "github.com/gin-gonic/gin"
 )
 
-func HelloWorld(c *gin.Context) {
+func blablabla(c *gin.Context) {
 	c.String(200, "im golang app. plz dont overload me")
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/", HelloWorld)
+	r.GET("/", blablabla)
 
 	err := r.Run(":8080")
 	if err != nil {
-		fmt.Print("linter drunk")
+		fmt.Print("drunk")
 	}
 
 }
